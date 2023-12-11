813f60df30002847b324aa6322b2b1cace3d56ff
BZR4 <BZR4@users.noreply.github.com>
2023-12-11 19:12:16 -0300

    ConsumerWidget

--- lib/features/entidades/presentation/pages/entity_page.dart
+++ lib/features/entidades/presentation/pages/entity_page.dart
@@ -1,11 +1,12 @@
 import 'package:f290_dsm_ldm_guiame_desktop_poc/features/entidades/presentation/pages/entity_form_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class EntityTypePage extends StatelessWidget {
+class EntityTypePage extends ConsumerWidget {
   const EntityTypePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tipos de Entidades'),
